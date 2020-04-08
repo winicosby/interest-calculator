919715ccfec271df6217f86c2f85e8da8c0ec42d
winie <61988443+winicosby@users.noreply.github.com>
2020-04-08 11:03:05 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+const data = [
+    {
+        principal: 2500,
+        time: 1.8
+    },
+    {
+        principal: 1000,
+        time: 5
+    },
+    {
+        principal: 3000,
+        time: 1
+    },
+    {
+        principal: 2000,
+        time: 3
+    },
+]
+
+
+function interestCalculator(data) {
+    const interestData = data.map((obj) => {
+        if (obj.principal >= 2500 && obj.time > 1 && obj.time < 3) {
+            obj.rate = 3;
+        } else if (obj.principal >= 2500 && obj.time >= 3) {
+            obj.rate = 4;
+        } else if (obj.principal <= 2500 && obj.time <= 1) {
+            obj.rate = 2;
+        } else {
+            obj.rate = 1;
+        }
+
+        obj.interest = (obj.principal * obj.rate * obj.time) / 100;
+        return obj
+    });
+
+    console.log(interestData);
+    return interestData;
+}
+
+interestCalculator(data);
